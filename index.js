e1d02a283eb3b9432208829e65f4b73655aa957a
Sarah Sami <sarah7sami@gmail.com>
2023-02-05 18:10:47 -0500

    schema.js for restaurant added using gql

---
+++ index.js
@@ -0,0 +1,11 @@
+mongoose.connect('mongodb://localhost/lab3_restaurant_database', {
+  useNewUrlParser: true,
+  useUnifiedTopology: true
+});
+
+const db = mongoose.connection;
+
+db.on('error', console.error.bind(console, 'connection error:'));
+db.once('open', function() {
+  console.log('Connected to MongoDB');
+});
